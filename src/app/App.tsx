bda5c90e5678f4a322e34af2a3eb26cf30489202
Rail Shaikhlislamov <gentlyne@gmail.com>
2025-09-28 04:55:47 +0500

    feat: add about me

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,9 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Раиль</p>
         <p>
-          Текст писать тут
+          36 лет, рост 176 см. Работаю удалённо (обычный график 11:00—20:00). Сейчас изучаю фронтенд с нуля — фокус на
+          React
         </p>
+        <p>Telegram: </p>
+        <a href="tg://resolve?domain=gentlyne">@gentlyne</a>
       </header>
     </div>
   );
