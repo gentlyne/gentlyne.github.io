ce0f88f3b8200006aede68b40fef87b753fa1245
Rail Shaikhlislamov <boec_rail@mail.ru>
2026-02-11 16:09:04 +0500

    fix: providers move to index.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ThemeProvider } from '../contexts/ThemeContext';
 import { Header } from 'src/shared/ui-kit/header/header';
-import { LanguageProvider } from 'src/contexts/LanguageContext';
 
 function App() {
   return (
-    <LanguageProvider>
-      <ThemeProvider>
-        <Header />
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>Раиль</p>
-            <p>
-              36 лет, рост 176 см. Работаю удалённо (обычный график 11:00—20:00). Сейчас изучаю фронтенд с нуля — фокус
-              React
-            </p>
-            <p>Telegram: </p>
-            <a href="tg://resolve?domain=gentlyne">@gentlyne</a>
-          </header>
-        </div>
-      </ThemeProvider>
-    </LanguageProvider>
+    <>
+      <Header />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Раиль</p>
+          <p>
+            36 лет, рост 176 см. Работаю удалённо (обычный график 11:00—20:00). Сейчас изучаю фронтенд с нуля — фокус
+            React
+          </p>
+          <p>Telegram: </p>
+          <a href="tg://resolve?domain=gentlyne">@gentlyne</a>
+        </header>
+      </div>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './app/index.css';
 import App from './app/App';
 
+import { ThemeProvider } from './contexts/ThemeContext';
+import { LanguageProvider } from './contexts/LanguageContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <LanguageProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </LanguageProvider>
   </React.StrictMode>
 );
